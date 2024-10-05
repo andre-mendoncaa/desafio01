c6069fca4a49c35c7df95d9225cbf67369e69423
Andre <andremendonca0504@gmail.com>
2024-10-04 22:31:05 -0300

    inicio

---
+++ src/Components/Button/index.js
@@ -0,0 +1,10 @@
+function Button() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
+  export default Button;
+  
---
+++ src/Components/Input/index.js
@@ -0,0 +1,12 @@
+import { InputContainer } from "./styles";
+
+const Input = () => {
+    return (
+      <InputContainer>
+        <input></input>
+      </InputContainer>
+    );
+  }
+  
+  export default Input;
+  
